64cb845fff95bbaadb37b7ef4a6098b50b785387
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-07-16 14:35:48 -0400

    commit tree 6ec8ee2b3bf5cbabda67e33775f59934d0254e9a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+   customerName = 'BOB'
+   return customerName
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob'
+
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+   bestCustomer = 'maybe bob'
+  return bestCustomer
+
+  // Feel free to move things around!
+}
+const leastFavoriteCustomer = 'kelvin';
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = 'darrely'
+  }
